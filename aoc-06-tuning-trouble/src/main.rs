e59809232545e42e550e95c0ff2ce58ba32deabe
Matias-Rossi <matiastadeorossi@gmail.com>
2022-12-14 22:36:16 -0300

    solve day 6 part 1

---
+++ aoc-06-tuning-trouble/src/main.rs
@@ -0,0 +1,51 @@
+use utils::input::read_lines; //Single 4KB line
+use std::{collections::HashSet, process::{self, exit}};
+
+fn main() {
+    if let Ok(lines) = read_lines("./resources/input.txt") {
+        let mut chars_read: usize = 0;
+        let mut last_chars_read: Vec<char> = Vec::new();
+
+        for line in lines {
+            if let Ok(text) = line {
+
+                for c in text.chars() {
+                    add_char_to_list(&mut last_chars_read, c);
+                    let values_found = are_chars_different(&last_chars_read);
+
+                    chars_read += 1;
+
+                    if values_found {
+                        println!("Found first start-of-packet marker after processing {} characters.", chars_read);
+                        exit(0);
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+fn add_char_to_list(list: &mut Vec<char>, character: char) -> Vec<char>{
+    list.insert(0, character);
+
+    if list.len() > 4 {
+        list.pop();
+    }
+
+    list.to_vec()
+}
+
+fn are_chars_different(list: &Vec<char>) -> bool {
+    if list.len() < 4 {
+        return false;
+    }
+
+    let mut set = HashSet::new();
+
+    for c in list.iter() {
+        set.insert(c);
+    }
+
+    set.len() == 4
+}
